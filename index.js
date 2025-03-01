b8d1f4556371d1f9dbafe03b0ab08c31c2e91627
AkshitaSrivastava0608 <akshita.sr752@gmail.com>
2025-03-01 21:35:27 +0530

    d1

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const state = {
     taskList: []
 }
@@ -19,7 +18,14 @@ const htmlTaskContent = ({id, url, title, description, type}) => `
         </button>
     </div>
     <div class="cardbody">
-        ${ url && `<img src ="${url}" alt ="card-img-top" class="card-img-top"/>`
+        ${ url ? `<img 
+            src ="${url}" 
+            alt ="card-img-top" 
+            class="card-img-top"/>`
+            :`<img 
+            src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScujirQqIFjN5GuM1565_-DIX6OyU_96HzNBl_BAX8GL0JzMs8&s" 
+            alt ="card-img-top" 
+            class="card-img-top"/>`
         }
         <h4 class="card-title"> ${title}</h4>
         <p class="card-text">${descritpion}></p>
@@ -28,8 +34,11 @@ const htmlTaskContent = ({id, url, title, description, type}) => `
         </div>
     </div>
     <div class="card-footer">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-         Open Task
+        <button type="button" 
+        class="btn btn-primary" 
+        data-bs-toggle="modal" 
+        data-bs-target="#showTask">
+        Open Task
         </button>
      </div>
   </div>
@@ -40,8 +49,12 @@ const htmlModalContent = ({id, url, title, description}) => {
  return `
  <div id=${id}>
  ${
-    url && `<img 
+    url ? `<img 
     src = "${url}" 
+    alt ="card-img-top" 
+    class="img-fluid"/>`
+    :`<img 
+    src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScujirQqIFjN5GuM1565_-DIX6OyU_96HzNBl_BAX8GL0JzMs8&s" 
     alt ="card-img-top" 
     class="img-fluid"/>`
 }
@@ -52,4 +65,49 @@ const htmlModalContent = ({id, url, title, description}) => {
  `
 
 }
-    
+const updateLocalStorage = () => {
+    localStorage.setItem('task', JSON.stringify({
+        tasks: state.taskList
+    }))
+}
+
+const loadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.task);
+
+    if(localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+    state.taskList.map((cardData)=> {
+        taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardData))
+    })
+}
+
+
+const handleSbmitBtn = (event) => {
+    const id = `${Date.now()}`;
+    const input = {
+        url: document.getElementById('ImageURL').value,
+        title: document.getElementById('taskTitle').value,
+        description: document.getElementById('taskDescription').value,
+        type: document.getElementById('tags').value,
+    }
+
+    if(input.title == "" || input.description == "" || input.type ==""){
+        return alert("Pls fill out all the neceesary fields")
+    }
+
+    taskContents.insertAdjacentHTML("beforeend", htmlTaskContent({...input, id})
+)
+
+state.taskList.push({...input, id})
+updateLocalStorage()
+
+}
+
+
+
+const openTask = (e) => {
+    if(!e) e = window.event;
+
+    const getTask = state.taskList.find(({id})=> id === e.target.id)
+    taskModal.innerHTML = htmlModalContent(getTask)
+}
